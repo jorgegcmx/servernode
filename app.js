4da355b83f50d8e41927fbab94d1b2333d29e057
jorgegcmx <jorgegcmx@gmail.com>
2019-05-24 10:44:56 -0500

    cambio de consuta sql

--- app.js
+++ app.js
@@ -345,7 +345,7 @@ app.get("/nutryfacil/:mes1/:mes2/:annio", function(req, res) {
     var request = new sql.Request();
     // query to the database and get the data
     request.query(
- "select MONTH(TranDate) as Fecha,CONVERT(VARCHAR, CAST(SUM(Qty)AS MONEY), 1)as kilos from INTran (nolock) where InvtID between 'PLL1000' and 'PLL1599' and MONTH(TranDate) between '" +
+ "select MONTH(TranDate) as Fecha,SUM(Qty)as kilos from INTran (nolock) where InvtID between 'PLL1000' and 'PLL1599' and MONTH(TranDate) between '" +
         req.params.mes1 + "' and '" +req.params.mes2 + "' and YEAR(TranDate)='" +req.params.annio + "' AND TranType='II' group by MONTH(TranDate) order by Fecha",
       function(err, recordsets) {
         if (err) {
@@ -370,7 +370,7 @@ app.get("/pollo10/:mes1/:mes2/:annio", function(req, res) {
     var request = new sql.Request();
     // query to the database and get the data
     request.query(
- "select MONTH(TranDate) as Fecha,CONVERT(VARCHAR, CAST(SUM(Qty)AS MONEY), 1)as kilos from INTran (nolock) where InvtID ='PLL0110'  and MONTH(TranDate) between '" +
+ "select MONTH(TranDate) as Fecha,SUM(Qty)as kilos from INTran (nolock) where InvtID ='PLL0110'  and MONTH(TranDate) between '" +
         req.params.mes1 + "' and '" +req.params.mes2 + "' and YEAR(TranDate)='" +req.params.annio + "' AND TranType='II' group by MONTH(TranDate) order by Fecha",
       function(err, recordsets) {
         if (err) {
@@ -394,7 +394,7 @@ app.get("/pollo9/:mes1/:mes2/:annio", function(req, res) {
     var request = new sql.Request();
     // query to the database and get the data
     request.query(
- "select MONTH(TranDate) as Fecha,CONVERT(VARCHAR, CAST(SUM(Qty)AS MONEY), 1)as kilos from INTran (nolock) where InvtID ='PLL0109'  and MONTH(TranDate) between '" +
+ "select MONTH(TranDate) as Fecha,SUM(Qty)as kilos from INTran (nolock) where InvtID ='PLL0109'  and MONTH(TranDate) between '" +
         req.params.mes1 + "' and '" +req.params.mes2 + "' and YEAR(TranDate)='" +req.params.annio + "' AND TranType='II' group by MONTH(TranDate) order by Fecha",
       function(err, recordsets) {
         if (err) {
